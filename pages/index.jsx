86ad85f8cb0457ffca0952d0d3e15cbd3fa93f7f
Ariel Pettyjohn <me@arielpettyjohn.com>
2022-10-22 17:59:09 -0700

    Refactor Resume styles

--- pages/index.jsx
+++ pages/index.jsx
@@ -29,64 +29,66 @@ export default function Resume () {
                 <link rel="icon" href="/favicon.ico" />
             </Head>
 
-            <section className='Resume'>
-                <Sidebar 
-                    contact       = {contact}
-                    skills        = {skills}
-                    interests     = {interests}
-                    profile       = {profile}
-                />
+            <div className='ResumeContainer'>
+                <section className='Resume'>
+                    <Sidebar 
+                        contact       = {contact}
+                        skills        = {skills}
+                        interests     = {interests}
+                        profile       = {profile}
+                    />
 
-                <div className='content'>
-                    <h1>Web Application Engineer + Developer</h1>
+                    <div className='content'>
+                        <h1>Web Application Engineer + Developer</h1>
 
-                    <section className='experiences'>
-                        <h2 >Experience</h2>
+                        <section className='experiences'>
+                            <h2 >Experience</h2>
 
-                        <aside className='experiences__summary'>
-                            <h3>Summary</h3>
-                            
-                            <List listItems={professionalSummary} />
-                        </aside>
+                            <aside className='experiences__summary'>
+                                <h3>Summary</h3>
+                                
+                                <List listItems={professionalSummary} />
+                            </aside>
 
-                        <section className='professionalExperiences'>
-                            <h3>Professional Experience</h3>
+                            <section className='professionalExperiences'>
+                                <h3>Professional Experience</h3>
 
-                            {professionalExperience.map((experience, index) => 
-                                <ProfessionalExperience 
-                                    key          = {index}
-                                    role         = {experience.role}
-                                    organization = {experience.organization}
-                                    beginDate    = {experience.beginDate}
-                                    endDate      = {experience.endDate}
-                                    location     = {experience.location}
-                                    summary      = {experience.summary}
-                                    clients      = {experience.clients}
-                                    skills       = {experience.skills}
-                                    achievements = {experience.achievements}
-                                /> 
-                            )}
+                                {professionalExperience.map((experience, index) => 
+                                    <ProfessionalExperience 
+                                        key          = {index}
+                                        role         = {experience.role}
+                                        organization = {experience.organization}
+                                        beginDate    = {experience.beginDate}
+                                        endDate      = {experience.endDate}
+                                        location     = {experience.location}
+                                        summary      = {experience.summary}
+                                        clients      = {experience.clients}
+                                        skills       = {experience.skills}
+                                        achievements = {experience.achievements}
+                                    /> 
+                                )}
+                            </section>
+
+                            <section className='academicExperiences'>
+                                <h3>Academic Experience</h3>
+
+                                {academicExperience.map((experience, index) => 
+                                    <ProfessionalExperience 
+                                        key          = {index}
+                                        organization = {experience.organization}
+                                        beginDate    = {experience.beginDate}
+                                        endDate      = {experience.endDate}
+                                        location     = {experience.location}
+                                        achievements = {experience.achievements}
+                                    /> 
+                                )}
+                            </section>
                         </section>
+                    </div>
+                </section>
 
-                        <section className='academicExperiences'>
-                            <h3>Academic Experience</h3>
-
-                            {academicExperience.map((experience, index) => 
-                                <ProfessionalExperience 
-                                    key          = {index}
-                                    organization = {experience.organization}
-                                    beginDate    = {experience.beginDate}
-                                    endDate      = {experience.endDate}
-                                    location     = {experience.location}
-                                    achievements = {experience.achievements}
-                                /> 
-                            )}
-                        </section>
-                    </section>
-                </div>
-            </section>
-
-            <div className='underlay'></div>
+                <div className='underlay'></div>
+            </div>
         </>
     )
 }
---
+++ pages/test.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import Head from 'next/head';
+
+export default function Test () {
+    return (
+        <>
+            <Head>
+                <title>Ariel Pettyjohn</title>
+
+                <meta name='description' content='Ariel Pettyjohn' />
+
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
+
+            <section className='Test'>
+                <h1>Ariel Pettyjohn</h1>
+                <h2>Web Application</h2>
+                <h3>Engineer + Developer</h3>
+            </section>
+        </>
+    )
+}
