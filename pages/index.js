f0c726351eb6b1fe106ff19439efee0df9545744
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-05-04 16:28:57 -0700

    Adjust for mobile

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,34 @@
-import Head from 'next/head';
+import Head from "next/head";
 
-import Resume from '../components/Resume/Resume';
+import Resume from "../components/Resume/Resume";
 
-import resume from '../content/resume';
+import resume from "../content/resume";
 
-import styles from '../styles/Home.module.scss';
+import styles from "../styles/Home.module.scss";
 
-export default function Home () {
-     return (
+export default function Home() {
+    return (
         <>
             <Head>
                 <title>Ariel Pettyjohn</title>
 
-                <meta name='description' content='Ariel Pettyjohn' />
+                <meta name="description" content="Ariel Pettyjohn" />
 
                 <link rel="icon" href="/favicon.ico" />
+
+                <meta
+                    name    = "viewport"
+                    content = "width=device-width, initial-scale=1.0, viewport-fit=cover"
+                />
             </Head>
 
             <small className={styles.downloadPDF}>
-                <a 
-                    href   = {resume.pdf}
-                    target = "_blank"
-                    rel    = "noreferrer"
-                >
+                <a href={resume.pdf} target="_blank" rel="noreferrer">
                     Download PDF
                 </a>
             </small>
 
             <Resume {...resume} />
         </>
-    )
+    );
 }
