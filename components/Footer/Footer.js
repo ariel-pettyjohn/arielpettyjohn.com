7a948ebe0fccf25e0eb8f2dbea00b0c975a029b5
Ariel Pettyjohn <me@arielpettyjohn.com>
2024-05-04 20:50:41 -0700

    Fix displayName error

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -6,22 +6,22 @@ import { getCurrentYear } from '../../util';
 
 import styles from "./Footer.module.scss";
 
-const Footer = forwardRef((_, ref) => {
-    return (
-        <footer className={styles.Footer} ref={ref}>
-            <section className={styles.getInTouch}>
-                <header>
-                    <h2 className="h5">Get in Touch</h2>
-                </header>
+export default function Footer () {
+    return forwardRef((_, ref) => {
+        return (
+            <footer className={styles.Footer} ref={ref}>
+                <section className={styles.getInTouch}>
+                    <header>
+                        <h2 className="h5">Get in Touch</h2>
+                    </header>
+                    
+                    <ContactForm />
+                </section>
                 
-                <ContactForm />
-            </section>
-            
-            <span className={styles.copyright}>
-                Copyright &copy; {getCurrentYear()} Ariel Pettyjohn 
-            </span>
-        </footer>
-    );
-});
-
-export default Footer;
+                <span className={styles.copyright}>
+                    Copyright &copy; {getCurrentYear()} Ariel Pettyjohn 
+                </span>
+            </footer>
+        );
+    });
+}
